d75ee24562f72da087e2ec21d54db7aaf444288e
Vinicius Lourenzoni <viniciuslourenzoni0@gmail.com>
2024-09-16 19:58:55 -0300

    initialize fetch stats

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,63 +1,29 @@
-import logo from "./assets/logo-goal-track.svg";
-import letsStart from "./assets/lets-start.svg";
-import { Plus, X } from "lucide-react";
-import { Button } from "./components/ui/button";
+
 import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-  DialogTrigger,
+  Dialog
 } from "./components/ui/dialog";
+import { CreateGoal } from "./components/create-goal";
+import { Summary } from "./components/summary";
+import { useEffect, useState } from "react";
+import { EmptyGoals } from "./components/empty-goals";
+
 export function App() {
+  const [summary, setSummary] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+      .then(response => response.json())
+      .then(data => {
+        setSummary(data.summary)
+      })
+  }, [])
+
   return (
     <Dialog>
-      <div className="h-screen flex flex-col items-center justify-center gap-8">
-        <img src={logo} alt="logo" />
-        <img src={letsStart} alt="lets start" />
-        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
-          mesmo?
-        </p>
 
-        <DialogTrigger asChild>
-          <Button>
-            <Plus className="size-4" />
-            Cadastrar meta
-          </Button>
-        </DialogTrigger>
-      </div>
+      {summary?.total > 0 ? <Summary /> : <EmptyGoals />}
 
-      <DialogContent>
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-col gap-3">
-            <div className="flex items-center justify-between">
-              <DialogTitle>Cadastrar meta</DialogTitle>
-              <DialogClose>
-                <X className="size-5 text-zinc-600" />
-              </DialogClose>
-            </div>
-            <DialogDescription>
-              Adicione atividades que te fazem bem e que você quer continuar
-              praticando toda semana.
-            </DialogDescription>
-          </div>
-
-          <form action="" className="flex-1 flex flex-col justify-between">
-            <div></div>
-
-            <div className="flex items-center gap-3">
-              <DialogClose asChild>
-                <Button className="flex-1" variant="secondary">
-                  Fechar
-                </Button>
-              </DialogClose>
-              <Button className="flex-1">Salvar</Button>
-            </div>
-          </form>
-        </div>
-      </DialogContent>
+      <CreateGoal />
     </Dialog>
   );
 }
